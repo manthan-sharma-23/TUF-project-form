afc1cdec335cae6c4f0ae239cfa9be652ea37556
Manthan Sharma <143496678+manthan-sharma-23@users.noreply.github.com>
2024-03-21 19:25:37 +0530

    healht

--- client/src/components/executionOutput.tsx
+++ client/src/components/executionOutput.tsx
@@ -19,13 +19,23 @@ const ExecutionOutput = () => {
           sx={{ border: "2px solid blue" }}
         >
           <div className="h-full w-full bg-blue-900 p-2 font-mono text-white font-medium text-lg">
-            {Object.keys(output).map((key) => (
-              <section className="flex w-full ml-4 mt-1" key={key}>
-                <p>{key}</p>
-                <p className="mx-2">:</p>
-                <p>{(output as Record<string, string>)[key]}</p>
-              </section>
-            ))}
+            <section className="flex w-full ml-4 mt-1">
+              <p>Output</p>
+              <p className="mx-2">:</p>
+              <p>{output.stdout}</p>
+            </section>
+            <section className="flex w-full ml-4 mt-1">
+              <p>Time Taken</p>
+              <p className="mx-2">:</p>
+              <p>{output.time + "s"}</p>
+            </section>
+            <section className="flex w-full ml-4 mt-1">
+              <p>Error</p>
+              <p className="mx-2">:</p>
+              <p className={`${output.stderr ? "text-red-600" : ""}`}>
+                {output.stderr ? output.stderr : null}
+              </p>
+            </section>
           </div>
         </Box>
       )}
