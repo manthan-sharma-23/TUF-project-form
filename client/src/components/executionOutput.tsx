e5fffb9fa497adf7e4c7284303cfd0bfac262bff
Manthan Sharma <143496678+manthan-sharma-23@users.noreply.github.com>
2024-03-21 19:20:34 +0530

    healht

--- client/src/components/executionOutput.tsx
+++ client/src/components/executionOutput.tsx
@@ -19,11 +19,11 @@ const ExecutionOutput = () => {
           sx={{ border: "2px solid blue" }}
         >
           <div className="h-full w-full bg-blue-900 p-2 font-mono text-white font-medium text-lg">
-            {Object.entries(output).map(([key, value]) => (
+            {Object.keys(output).map((key) => (
               <section className="flex w-full ml-4 mt-1" key={key}>
                 <p>{key}</p>
                 <p className="mx-2">:</p>
-                <p>{value as string}</p>
+                <p>{(output as Record<string, string>)[key]}</p>
               </section>
             ))}
           </div>
